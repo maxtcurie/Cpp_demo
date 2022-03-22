223c191a1f93da5081846e037e4f200ed79b5363
maxcurie1996 <maxcurie@utexas.edu>
2022-03-22 12:26:39 -0700

    3

---
+++ Matrix/Random_maxtrix_generation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+float mul(float a ,float b){
+	a
+}
+
+int main(){
+int n=10;
+float a[n][n],b[n][n],mul[n][n];
+
+for(int i=0; i<n; i++){
+	for(int j=0;j<n;j++){
+		a[i][j]=rand();
+		b[i][j]=rand();		
+	}
+}
+
+}
